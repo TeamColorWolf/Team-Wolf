1ddf5bb12823cbbd28753c35596f8dbf81279e52
xuyuanjun <610658272@qq.com>
2014-11-11 19:00:38 +0800

    加入stockblcontroller

---
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/stockBL/StockBLController.java
@@ -0,0 +1,27 @@
+package TeamWolf.TeamWolf.client.BL.stockBL;
+
+import TeamWolf.TeamWolf.client.vo.TypeList;
+import TeamWolf.TeamWolf.client.vo.TypeVO;
+
+public class StockBLController {
+
+	public int addType(TypeVO t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int delType(TypeVO t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+    
+	public int updType(TypeVO t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public TypeList shoAllType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
