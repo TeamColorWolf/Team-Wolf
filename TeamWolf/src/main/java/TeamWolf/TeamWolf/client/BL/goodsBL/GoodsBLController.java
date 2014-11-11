3396fb42b4c2fccd58bd34d152c9201ad7dcbab8
xuyuanjun <610658272@qq.com>
2014-11-11 18:54:37 +0800

    增加controller

---
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/goodsBL/GoodsBLController.java
@@ -0,0 +1,61 @@
+package TeamWolf.TeamWolf.client.BL.goodsBL;
+import TeamWolf.TeamWolf.client.vo.*;
+
+public class GoodsBLController {
+
+
+	
+	public int addGood(GoodsVO g){
+		return 0;
+	}
+	public int delGood(GoodsVO g){
+		return 0;
+	}
+	public int updGood(GoodsVO g){
+		return 0;
+	}
+	public int finGood(GoodsVO g){
+		return 0;
+	}
+	public GoodList shoGood(GoodsVO g){
+		return null;
+	}
+	public GoodsStockList shoStockList(int beginDate, int endDate){
+		return null;
+	}
+	public GoodsStockList shoStockDaily(){
+		return null;
+	}
+	public int goodsExport(/*���۵�����*/){
+		return 0;
+	}
+	public int goodsExportReturn(/*�����˻�������*/){
+		return 0;
+	}
+	public int goodsImport(/*���������*/){
+		return 0;
+	}
+	public int goodsImportReturn(/*����˻�������*/){
+		return 0;
+	}
+	public int PresentList(GoodsVO g){
+		return 0;
+	}
+	public int handlePresentList(PresentList p){
+		return 0;
+	}
+	public int setGoodWL(GoodsVO g){
+		return 0;
+	}
+	public int increaseGood(GoodsVO g){
+		return 0;
+	}
+	public int decreaseGood(GoodsVO g){
+		return 0;
+	}
+	public int MonitoringWL(GoodsVO g){
+		return 0;
+	}
+	
+	
+}
