9d3d1890e6cd39b7e53e2e3d4d7f016aa983ffa9
HalaWKS <1028276446@qq.com>
2014-11-12 09:04:10 +0800

    。。。

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/applicationBL/ApplicationBL.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/applicationBL/ApplicationBL.java
@@ -10,5 +10,4 @@ public class ApplicationBL {
 		return null;
 	}
 	
-	
 }
---
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/saleBL/SaleBLAssistant.java
@@ -0,0 +1,37 @@
+package TeamWolf.TeamWolf.client.BL.saleBL;
+
+import java.util.Date;
+
+import TeamWolf.TeamWolf.client.vo.CustomerVO;
+import TeamWolf.TeamWolf.client.vo.ImportListVO;
+import TeamWolf.TeamWolf.client.vo.ImportRejectListVO;
+import TeamWolf.TeamWolf.client.vo.SaleListVO;
+import TeamWolf.TeamWolf.client.vo.SaleRejectListVO;
+
+public class SaleBLAssistant {
+	
+	public int canAddCustomer (CustomerVO cvo){
+		return 0;
+	}
+	
+	public int canAddImport (ImportListVO ivo){
+		return 0;
+	}
+	
+	public int canAddImportReject (ImportRejectListVO irvo){
+		return 0;
+	}
+	
+	public int canAddSale (SaleListVO svo){
+		return 0;
+	}
+	
+	public int canAddSaleReject (SaleRejectListVO srvo){
+		return 0;
+	}
+	
+	public Date getDate (){
+		return null;
+	}
+	
+}
