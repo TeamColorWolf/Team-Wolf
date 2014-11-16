1d210214c859f58fe199b4375a35ca9e64ffe7f0
yuqiGao <574057633@qq.com>
2014-11-16 14:27:26 +0800

    手贱了

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BLservice/financeBLservice/EventviewBlservice.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BLservice/financeBLservice/EventviewBlservice.java
@@ -1,5 +1,9 @@
 package TeamWolf.TeamWolf.client.BLservice.financeBLservice;
 
+import TeamWolf.TeamWolf.client.vo.TimeVO;
+
 public interface EventviewBlservice {
      public int event();
+     public int eventDATE(TimeVO time1, TimeVO time2);
+     public int eventPERIOD();
 }
