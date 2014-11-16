d558f25cbbaede79eeee61476c9d5e7f8cab7593
yuqiGao <574057633@qq.com>
2014-11-16 14:21:51 +0800

    补充

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/financeBL/Table.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/financeBL/Table.java
@@ -1,6 +1,6 @@
 package TeamWolf.TeamWolf.client.BL.financeBL;
 
-import TeamWolf.TeamWolf.client.BL.applicationBL.forFinance.FinanceApplicationController;
+import TeamWolf.TeamWolf.client.BL.applicationBL.forFinance.FinanceSubmit;
 import TeamWolf.TeamWolf.client.BLservice.financeBLservice.TableBlservice;
 import TeamWolf.TeamWolf.client.DATAservice.applicationDATAservice.FinanceApplicationDATAservice;
 import TeamWolf.TeamWolf.client.vo.CashApplicationVO;
@@ -9,25 +9,22 @@ import TeamWolf.TeamWolf.client.vo.RecieptApplicationVO;
 
 public class Table implements TableBlservice{
 
-	FinanceApplicationDATAservice fads;
+	FinanceSubmit fs;
 	
 	public Table(String iP) {
 		// TODO 自动生成的构造函数存根
 	}
 
 	public int CreateReceipt(RecieptApplicationVO rvo) {
-		// TODO 自动生成的方法存根
-		return 0;
+		return fs.submitRecieptApplication(rvo);
 	}
 
 	public int CreatePayment(PaymentApplicationVO pvo) {
-		// TODO 自动生成的方法存根
-		return 0;
+		return fs.submitPaymentApplication(pvo);
 	}
 
 	public int CreateCash(CashApplicationVO cvo) {
-		// TODO 自动生成的方法存根
-		return 0;
+		return fs.submitCashApplication(cvo);
 	}
 
 
