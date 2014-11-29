1333594e3fd0a05159754bec05413dd9a4637b0e
yuqiGao <574057633@qq.com>
2014-11-30 00:12:25 +0800

    任务好多啊

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/server/saleDATA/CustomerDATA.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/server/saleDATA/CustomerDATA.java
@@ -1,20 +1,55 @@
 package TeamWolf.TeamWolf.server.saleDATA;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 import TeamWolf.TeamWolf.client.DATAservice.customerDATAservice.CustomerDATAservice;
 import TeamWolf.TeamWolf.client.po.CustomerPO;
+import TeamWolf.TeamWolf.client.po.UserPO;
+import TeamWolf.TeamWolf.server.FileName;
+import TeamWolf.TeamWolf.server.FileOpr;
 
-public class CustomerDATA implements CustomerDATAservice{
-
+public class CustomerDATA extends UnicastRemoteObject implements CustomerDATAservice{
+	static ArrayList<CustomerPO> list = null;
+	public CustomerDATA() throws RemoteException{
+		super();
+		// TODO Auto-generated constructor stub
+		this.getList();
+		if(list == null){
+			list = new ArrayList<CustomerPO>();
+		}
+		
+	}
+	
 	public int addCustomer(CustomerPO cpo) throws RemoteException {
 		// TODO Auto-generated method stub
+		list.add(cpo);
+		try {
+			FileOpr.writeFile(FileName.userFile, list);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 30001;
+		}
 		return 0;
 	}
 
 	public int delCustomer(CustomerPO cpo) throws RemoteException {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).getNum().equals(cpo.getNum())){
+				list.remove(i);
+				try {
+					FileOpr.writeFile(FileName.userFile, list);
+					return 0;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return 30002;
+				}
+			}
+		}
 		return 0;
 	}
 
@@ -24,19 +59,48 @@ public class CustomerDATA implements CustomerDATAservice{
 	}
 
 	public int modCustomer(CustomerPO po, CustomerPO newpo) throws RemoteException {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).getNum().equals(po.getName())){
+			  list.remove(list.get(i));
+			  list.add(i,newpo);
+				try {
+					FileOpr.writeFile(FileName.userFile, list);
+					return 0;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return 30003;
+				}
+			}
+		}
 		return 0;
 	}
 
 	public CustomerPO findCustomer(String name, String number)
 			throws RemoteException {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).getNum().equals(number)||list.get(i).getName().equals(name)){
+				return list.get(i);
+			}
+		}
 		return null;
 	}
 
 	public ArrayList<String> getCustomerList() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private void getList(){
+		try {
+			list = (ArrayList<CustomerPO>) FileOpr.readFile(FileName.customerFile);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
