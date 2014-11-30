09df510bc077ffb03df07e106166682f6bdc406a
whj0401 <emls0401@163.com>
2014-11-30 19:48:31 +0800

    还好备份了

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/manageUI/ApprovePanel.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/manageUI/ApprovePanel.java
@@ -2,10 +2,20 @@ package TeamWolf.TeamWolf.client.GUI.manageUI;
 
 import javax.swing.JPanel;
 
-public class ApprovePanel extends JPanel{
+import TeamWolf.TeamWolf.client.BL.applicationBL.ManageApproveService;
+import TeamWolf.TeamWolf.client.BL.applicationBL.forManage.ManageApproveController;
 
+public class ApprovePanel extends JPanel{
+	static ManageApproveService service = new ManageApproveController(ManageFrame.IP);
+	
+	SaleApprovePanel approve;
+	
 	public ApprovePanel(){
 		super();
+		
+		approve = new SaleApprovePanel();
+		
+		this.add(approve);
 		
 		this.setLayout(null);
 		this.setSize(ManageFrame.width, ManageFrame.height-ManageFrame.sho);
--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/manageUI/SaleApprovePanel.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/manageUI/SaleApprovePanel.java
@@ -2,47 +2,73 @@ package TeamWolf.TeamWolf.client.GUI.manageUI;
 
 import java.util.ArrayList;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import TeamWolf.TeamWolf.client.BL.applicationBL.ManageApproveService;
 import TeamWolf.TeamWolf.client.vo.ApplicationVO;
 
 public class SaleApprovePanel extends JPanel{
-	ManageApproveService service;
+	ManageApproveService service = ApprovePanel.service;
 	ArrayList<ApplicationVO> list;
 	
-	
 	static String[] column = {"单据类型", "单据编号", "操作员", "审批结果"};
+	DefaultTableModel tableModel = new DefaultTableModel();
+	TableColumn approve;
 	JTable table;
 	JScrollPane scroll;
 	Object[][] content;
 	
 	JComboBox<String> choice = new JComboBox<String>();
 	
-	private final static int width = ManageFrame.width;
+	private final static int width = ManageFrame.width/2;
 	private final static int height = ManageFrame.height-ManageFrame.sho;
 	private final static int rowH = 25;
 	private final static int tableW = 450;
 	
 	public SaleApprovePanel(){
+		choice.addItem("通过");
+		choice.addItem("驳回");
+		choice.addItem("搁置");
+		this.getContent();
+		tableModel.setDataVector(content, column);
+		table = new JTable(tableModel);
+		table.setRowHeight(rowH);
+		approve = table.getColumnModel().getColumn(3);
+		approve.setCellEditor(new DefaultCellEditor(choice));
 		
+		scroll = new JScrollPane(table);
+		scroll.setSize(tableW, height);
+		scroll.setLocation(0, 0);
+		
+		this.add(scroll);
+		
+		this.setSize(width, height);
+		this.setLocation(0, 0);
+		this.setLayout(null);
+		this.setVisible(true);
 	}
 	
 	private void getContent(){
-		list = service.getUnsetApplicationList();
-		if(list.size() < 16){
-			content = new Object[16][4];
+//		list = service.getUnsetApplicationList();//TODO 
+		if(list == null || list.size() < 20){
+			content = new Object[20][4];
 		}
 		else{
 			content = new Object[list.size()][4];
 		}
-		for(int i = 0; i < list.size(); i++){
-			ApplicationVO vo = list.get(i);
-			String[] n = vo.number.split("-");
-			
+		if(list != null){
+			for(int i = 0; i < list.size(); i++){
+				ApplicationVO vo = list.get(i);
+				content[i][0] = ApplicationType.getType(vo.number);
+				content[i][1] = vo.number;
+				content[i][2] = vo.operator;
+			}
 		}
 	}
 }
