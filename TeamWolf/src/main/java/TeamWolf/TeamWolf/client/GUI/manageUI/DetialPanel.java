141d75ba54391a2f1ffd23ab7443728c8c4ce483
whj0401 <emls0401@163.com>
2014-11-30 23:05:39 +0800

    do nothing

---
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/manageUI/DetialPanel.java
@@ -0,0 +1,9 @@
+package TeamWolf.TeamWolf.client.GUI.manageUI;
+
+import javax.swing.JPanel;
+
+public class DetialPanel extends JPanel{
+	
+	static String[] goodsList = {"编号", "名称", "型号", "数量", "单价", "总额"};
+	
+}
