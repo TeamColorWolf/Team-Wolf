81d78c6b9ba3f043764176e9594845eb593c91be
xuyuanjun <610658272@qq.com>
2014-12-01 16:59:36 +0800

    yiku

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/vo/IncreaseToMatchVO.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/vo/IncreaseToMatchVO.java
@@ -1,15 +1,20 @@
 package TeamWolf.TeamWolf.client.vo;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class IncreaseToMatchVO extends ApplicationVO {
 
 	 GoodsVO toIncrease;
+	 String time;
 		
      public IncreaseToMatchVO(GoodsVO toIncrease){
    	      this.toIncrease=toIncrease; 
+   	      this.time=this.getPresentTime();
      }
      
      public String getInfo(){
-   	  return toIncrease.getNumber()+" "+toIncrease.getName()+" "+toIncrease.getModel()+" 报溢数量"+toIncrease.getAmount();
+   	  return time+" "+toIncrease.getNumber()+" "+toIncrease.getName()+" "+toIncrease.getModel()+" 报溢数量"+toIncrease.getAmount();
      }
      
      public String getNumber(){
@@ -27,5 +32,14 @@ public class IncreaseToMatchVO extends ApplicationVO {
      public String getAmount(){
    	  return ""+toIncrease.getAmount();
      }
+     
+     private String getPresentTime(){
+ 		//获得当前时间
+ 		Calendar c=Calendar.getInstance();
+ 		SimpleDateFormat s=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+ 		//SimpleDateFormat s=new SimpleDateFormat("MM-dd HH:mm:ss");
+ 		String time=s.format(c.getTime());
+ 		return time;
+ 	}
      
 }
