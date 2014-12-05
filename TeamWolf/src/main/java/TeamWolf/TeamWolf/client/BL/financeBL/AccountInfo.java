789b2f4b44afae39734ae425363f3b944e308cbd
yuqiGao <574057633@qq.com>
2014-12-05 20:21:18 +0800

    先推一下

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/financeBL/AccountInfo.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/financeBL/AccountInfo.java
@@ -2,24 +2,32 @@ package TeamWolf.TeamWolf.client.BL.financeBL;
 
 import java.util.ArrayList;
 
+import TeamWolf.TeamWolf.client.DATAservice.financeDATAservice.financeDATAservice;
 import TeamWolf.TeamWolf.client.vo.CashApplicationVO;
 import TeamWolf.TeamWolf.client.vo.PaymentApplicationVO;
 import TeamWolf.TeamWolf.client.vo.RecieptApplicationVO;
 import TeamWolf.TeamWolf.client.vo.financeVO;
 
 public class AccountInfo implements AccountInfoBLservice{
+	
+	public String URL = "";
+	public static financeDATAservice fds;
 
-	public int RecieptMod(RecieptApplicationVO volist) {
+	public AccountInfo(String IP){
+		URL = "rmi://" + IP + "/financeDATAservice";
+	}
+	
+	public int RecieptMod(RecieptApplicationVO vo) {
+		ArrayList<financeVO> volist = vo.getAccountList();
+		return 0;
+	}
+
+	public int PaymentMod(PaymentApplicationVO vo) {
 		// TODO 自动生成的方法存根
 		return 0;
 	}
 
-	public int PaymentMod(PaymentApplicationVO volist) {
-		// TODO 自动生成的方法存根
-		return 0;
-	}
-
-	public int CashMod(CashApplicationVO volist) {
+	public int CashMod(CashApplicationVO vo) {
 		// TODO 自动生成的方法存根
 		return 0;
 	}
--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/financeBL/AccountInfoBLservice.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/BL/financeBL/AccountInfoBLservice.java
@@ -8,7 +8,7 @@ import TeamWolf.TeamWolf.client.vo.RecieptApplicationVO;
 
 public interface AccountInfoBLservice {
 
-	public int RecieptMod(RecieptApplicationVO volist);
-	public int PaymentMod(PaymentApplicationVO volist);
-	public int CashMod(CashApplicationVO volist);
+	public int RecieptMod(RecieptApplicationVO vo);
+	public int PaymentMod(PaymentApplicationVO vo);
+	public int CashMod(CashApplicationVO vo);
 }
