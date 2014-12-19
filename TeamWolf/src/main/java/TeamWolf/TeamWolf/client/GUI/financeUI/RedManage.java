ef6891d650d821f83e859042cf5518a405077c45
yuqiGao <574057633@qq.com>
2014-12-19 15:18:06 +0800

    Sale单据的红冲

--- TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/financeUI/RedManage.java
+++ TeamWolf/src/main/java/TeamWolf/TeamWolf/client/GUI/financeUI/RedManage.java
@@ -2,6 +2,10 @@ package TeamWolf.TeamWolf.client.GUI.financeUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -11,27 +15,52 @@ import TeamWolf.TeamWolf.ErrorTW;
 import TeamWolf.TeamWolf.client.BL.applicationBL.FinanceApplicationService;
 import TeamWolf.TeamWolf.client.BL.applicationBL.forFinance.FinanceApplicationController;
 import TeamWolf.TeamWolf.client.BL.applicationBL.forFinance.RecieptApplication;
+import TeamWolf.TeamWolf.client.BL.customerBL.CustomerOpr;
+import TeamWolf.TeamWolf.client.BL.customerBL.CustomerOprBLservice;
 import TeamWolf.TeamWolf.client.BLservice.tableInquireBLservice.TableInquireBLservice;
+import TeamWolf.TeamWolf.client.DATAservice.applicationDATAservice.FinanceApplicationDATAservice;
+import TeamWolf.TeamWolf.client.DATAservice.applicationDATAservice.SaleApplicationDATAservice;
+import TeamWolf.TeamWolf.client.DATAservice.applicationDATAservice.StockApplicationDATAservice;
 import TeamWolf.TeamWolf.client.GUI.messageUI.MessageFrame;
 import TeamWolf.TeamWolf.client.GUI.tableInquireUI.RunProcessPanel;
 import TeamWolf.TeamWolf.client.GUI.tableInquireUI.TableInquirePanel;
+import TeamWolf.TeamWolf.client.po.GoodsPO;
+import TeamWolf.TeamWolf.client.po.ImportListPO;
+import TeamWolf.TeamWolf.client.po.ImportRejectListPO;
+import TeamWolf.TeamWolf.client.po.SaleListPO;
+import TeamWolf.TeamWolf.client.po.SaleRejectListPO;
 import TeamWolf.TeamWolf.client.vo.CustomerVO;
+import TeamWolf.TeamWolf.client.vo.ImportListVO;
 import TeamWolf.TeamWolf.client.vo.RecieptApplicationVO;
 import TeamWolf.TeamWolf.client.vo.RunProcessVO;
+import TeamWolf.TeamWolf.client.vo.SaleListVO;
 import TeamWolf.TeamWolf.client.vo.financeVO;
+import TeamWolf.TeamWolf.server.applicationDATA.SaleApplicationDATA;
+import TeamWolf.TeamWolf.server.applicationDATA.StockApplicationDATA;
 
 public class RedManage {
 	
 	  TableInquireBLservice service = TableInquirePanel.service;
-	  public Object[][] content;
+	  Object[][] content;
 	  ArrayList<RunProcessVO> AppVOList;
+	  SaleApplicationDATAservice saleads;
+	  FinanceApplicationDATAservice fads;
+	  StockApplicationDATAservice stockads;
 	  String IP;
+	  String URLsale;
+	  String URLfinance;
+	  String URLstock;
 	  private static RunProcessPanel panel ;
 	  private final static int BW = 80;
 	  private final static int BH = 25;
 	
       public RedManage(String IP,RunProcessPanel Inputpanel){
     	  
+    	  URLsale ="rmi://" + IP + "/saleApplicationDATAservice";
+    	  URLfinance = "rmi://" + IP + "/financeApplicationDATAservice";
+    	  URLstock = "rmi://" + IP + "/stockApplicationDATAservice";
+    	  DATAfactory();
+    	
     	  AppVOList = new ArrayList<RunProcessVO>();
     	  panel = new RunProcessPanel();
     	  this.panel = Inputpanel;
@@ -42,6 +71,20 @@ public class RedManage {
     	  rdBut.addActionListener(new RedCheck());
     	  panel.add(rdBut);
       }
+      
+    public void DATAfactory(){
+    	  try {
+  			saleads = (SaleApplicationDATAservice) Naming.lookup(URLsale);
+  			fads = (FinanceApplicationDATAservice) Naming.lookup(URLfinance);
+  			stockads = (StockApplicationDATAservice) Naming.lookup(URLstock);
+  		} catch (MalformedURLException e) {
+  			e.printStackTrace();
+  		} catch (RemoteException e) {
+  			e.printStackTrace();
+  		} catch (NotBoundException e) {
+  			e.printStackTrace();
+  		}
+    }  
       
     public void getContent(){
 	    ArrayList<RunProcessVO> list = service.runProcess(panel.timeSelect.getBeginVO(),panel.timeSelect.getEndVO());
@@ -84,16 +127,157 @@ public class RedManage {
         	AppType = AppTypeArray[0];
         }else{
         	return -1;
+        }      
+//---------------------------------------------------------------------------------------------------------        
+        
+        if(AppType.equals("JHD")){
+        	String NUMBER = AppVOList.get(0).number;
+        	ArrayList<ImportListPO> poList = new ArrayList<ImportListPO>();
+        	ImportListPO old = null;
+        	try {
+				poList= saleads.getImportList();
+				for(int i = 0;i<poList.size();i++){
+					if(NUMBER.equals(poList.get(i).number)){
+						old = poList.get(i);
+						break;
+					}
+				}
+				if(old == null){
+					MessageFrame mf = new MessageFrame(ErrorTW.webError);
+					return -1;
+				}
+			    String []numberAdjustArray = old.number.split("-");
+			    double numberAdjust = Double.parseDouble(numberAdjustArray[2])+1;
+			    numberAdjustArray[2] = ""+numberAdjust;
+			    String newNumber = numberAdjustArray.toString();
+			    old.number = newNumber;
+			    ArrayList<GoodsPO> goodsList = old.getGoodsList();
+			    for(int i=0;i<goodsList.size();i++){
+			    	String oldNumber = goodsList.get(i).getNumber();
+			    	String oppsiteNumber = ""+(Double.parseDouble(oldNumber)*(-1));
+			    	goodsList.get(i).setNumber(oppsiteNumber);
+			    }
+			    old.setGoodsList(goodsList);
+				saleads.submitImportList(old);
+				saleads.approvalImportList(old);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
         }
-        if(AppType.equals("SKD")){
-    	    ArrayList<financeVO> accountList = new ArrayList<financeVO>();
-    	 //   ArrayList<String> moneyList = new 
-		    
-        	//RecieptApplicationVO vo = new RecieptApplicationVO(accountList, moneyList, number, operator, note, customer)
-        	
-        	//RecieptApplication fa = new RecieptApplication(vo, AppType);
-        	
+        
+ //-------------------------------------------------------------------------------------------------------       
+        
+        else if(AppType.equals("JHTHD")){
+        	String NUMBER = AppVOList.get(0).number;
+        	ArrayList<ImportRejectListPO> poList = new ArrayList<ImportRejectListPO>();
+        	ImportRejectListPO old = null;
+        	try {
+				poList= saleads.getImportRejectList();
+				for(int i = 0;i<poList.size();i++){
+					if(NUMBER.equals(poList.get(i).number)){
+						old = poList.get(i);
+						break;
+					}
+				}
+				if(old == null){
+					MessageFrame mf = new MessageFrame(ErrorTW.webError);
+					return -1;
+				}
+			    String []numberAdjustArray = old.number.split("-");
+			    double numberAdjust = Double.parseDouble(numberAdjustArray[2])+1;
+			    numberAdjustArray[2] = ""+numberAdjust;
+			    String newNumber = numberAdjustArray.toString();
+			    old.number = newNumber;
+			    ArrayList<GoodsPO> goodsList = old.getGoodsList();
+			    for(int i=0;i<goodsList.size();i++){
+			    	String oldNumber = goodsList.get(i).getNumber();
+			    	String oppsiteNumber = ""+(Double.parseDouble(oldNumber)*(-1));
+			    	goodsList.get(i).setNumber(oppsiteNumber);
+			    }
+			    old.setGoodsList(goodsList);
+				saleads.submitImportRejectList(old);
+				saleads.approvalImportRejectList(old);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
         }
+ //---------------------------------------------------------------------------------------------------       
+        else if(AppType.equals("XSD")){
+        	String NUMBER = AppVOList.get(0).number;
+        	ArrayList<SaleListPO> poList = new ArrayList<SaleListPO>();
+        	SaleListPO old = null;
+        	try {
+				poList= saleads.getSaleList();
+				for(int i = 0;i<poList.size();i++){
+					if(NUMBER.equals(poList.get(i).number)){
+						old = poList.get(i);
+						break;
+					}
+				}
+				if(old == null){
+					MessageFrame mf = new MessageFrame(ErrorTW.webError);
+					return -1;
+				}
+			    String []numberAdjustArray = old.number.split("-");
+			    double numberAdjust = Double.parseDouble(numberAdjustArray[2])+1;
+			    numberAdjustArray[2] = ""+numberAdjust;
+			    String newNumber = numberAdjustArray.toString();
+			    old.number = newNumber;
+			    ArrayList<GoodsPO> goodsList = old.getGoodsList();
+			    for(int i=0;i<goodsList.size();i++){
+			    	String oldNumber = goodsList.get(i).getNumber();
+			    	String oppsiteNumber = ""+(Double.parseDouble(oldNumber)*(-1));
+			    	goodsList.get(i).setNumber(oppsiteNumber);
+			    }
+			    old.setGoodsList(goodsList);
+				saleads.submitExportList(old);
+				saleads.approvalExportList(old);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+        }
+//------------------------------------------------------------------------------------------------------ 
+        
+        else if(AppType.equals("XSTHD")){
+        	String NUMBER = AppVOList.get(0).number;
+        	ArrayList<SaleRejectListPO> poList = new ArrayList<SaleRejectListPO>();
+        	SaleRejectListPO old = null;
+        	try {
+				poList= saleads.getSaleRejectList();
+				for(int i = 0;i<poList.size();i++){
+					if(NUMBER.equals(poList.get(i).number)){
+						old = poList.get(i);
+						break;
+					}
+				}
+				if(old == null){
+					MessageFrame mf = new MessageFrame(ErrorTW.webError);
+					return -1;
+				}
+			    String []numberAdjustArray = old.number.split("-");
+			    double numberAdjust = Double.parseDouble(numberAdjustArray[2])+1;
+			    numberAdjustArray[2] = ""+numberAdjust;
+			    String newNumber = numberAdjustArray.toString();
+			    old.number = newNumber;
+			    ArrayList<GoodsPO> goodsList = old.getGoodsList();
+			    for(int i=0;i<goodsList.size();i++){
+			    	String oldNumber = goodsList.get(i).getNumber();
+			    	String oppsiteNumber = ""+(Double.parseDouble(oldNumber)*(-1));
+			    	goodsList.get(i).setNumber(oppsiteNumber);
+			    }
+			    old.setGoodsList(goodsList);
+				saleads.submitExportRejectList(old);
+				saleads.submitExportRejectList(old);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+        }
+        
+//------------------------------------------------------------------------------------------------------
+        
+        
+        
+//------------------------------------------------------------------------------------------------------        
     	return 0;
     }
       
